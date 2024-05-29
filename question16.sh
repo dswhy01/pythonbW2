16358bfdd443c1dffb40898f0166d5812df4ab80
HYWONG-00 <huiying1003@hotmail.com>
2024-05-29 17:22:49 +0930

    bnext

--- question16.sh
+++ question16.sh
@@ -5,10 +5,17 @@ git clone https://github.com/ian-knight-uofa/git-practice-02
 cd git-practice-02
 git checkout branch3
 git fetch origin branch3
+git pull origin branch3
+# force update the local branch(in case cannot see changes)
+git reset --hard origin/branch2
 
 git checkout branch2
-git merge branch3
+git fetch origin branch2
+git pull origin branch2
 
-git branch -d branch3
+git merge branch3 
+
+git branch -D branch3
+
 
 
